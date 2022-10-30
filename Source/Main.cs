753cd3974414821b71b041c23033992e6c1743ef
goog <just pm me bro>
2022-10-30 12:37:16 -0700

    comment out logs, added new check so that bots will charge to at least have a full day worth of charge before getting kicked off a charger

--- Source/Main.cs
+++ Source/Main.cs
@@ -2,6 +2,7 @@
 using HarmonyLib;
 using Verse;
 using RimWorld;
+using System;
 
 namespace FriendlyBotsDontWander
 {
@@ -44,12 +45,25 @@ namespace FriendlyBotsDontWander
     {
         public static void Postfix(Building_MechCharger __instance, ref Pawn ___currentlyChargingMech)
         {
+            
 
             // every 625 ticks (15 in game minutes), kick the bot off the charger so it will do work if available
             // if none is available, it will just start charging again
-            if (Find.TickManager.TicksGame % 625 == 0 && ___currentlyChargingMech != null)
+
+            
+            if (Find.TickManager.TicksGame % 625 == 0 
+                && ___currentlyChargingMech != null
+                // don't interrupt a bot if it doesnt have at least 10% more than minimum charge OR 90%, whichever is lower (to account for someone choosing to set a minimum charge >90% for some reason).
+                && ___currentlyChargingMech.needs.energy.CurLevelPercentage > Math.Min(.90f, (___currentlyChargingMech.GetMechControlGroup().mechRechargeThresholds.min + .10f)))
             {
-                Log.Message("Trying to stop pawn from charging... " + ___currentlyChargingMech.Name);
+                float blah = ___currentlyChargingMech.GetMechControlGroup().mechRechargeThresholds.min + .10f;
+
+                //Log.Message("Mech charge: " + ___currentlyChargingMech.needs.energy.CurLevelPercentage);
+                //Log.Message("Mech charge threshhold: " + ___currentlyChargingMech.GetMechControlGroup().mechRechargeThresholds.min);
+                //Log.Message("Mech charge threshhold after edit: " + blah);
+
+
+                //Log.Message("Trying to stop pawn from charging... " + ___currentlyChargingMech.Name);
                 ___currentlyChargingMech.jobs.curDriver.ReadyForNextToil();
                 //__instance.StopCharging();
             }
